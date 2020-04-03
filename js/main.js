6e679e3a82d5d15e8a086ebb4fc72d1d8f0efea8
Daniela Maria Volletti <danielavolletti@gmail.com>
2020-04-03 19:06:18 +0200

    creazione 64 quadrati

--- js/main.js
+++ js/main.js
@@ -5,44 +5,40 @@
 $(document).ready(function(){
 
   // creo div contenitori
-  var quadrato = $('.tablecontainer').prepend().html('<div class="square"></div>');
-
-
-
-
-  // // creo 64 contenitori
-  // var quadratiCompleti = parseInt(quadrato * 64);
-  //
-  // // aggiungo la classe square a tutti i quadrati
-  // quadratiCompleti.addClass('square');
-
-
-
-
-  // variabile per tenere il punteggio dei Verdi VS Rossi
-  var redPoint = 0, greenPoint = 0;
-
-  // cosa accade quando clicco sul quadrato
-  $('.square').click(
-    function () {
-      if($(this).hasClass('active')){
-        console.log("OH! l'hai già cliccato!");
-      } else if($(this).hasClass('redWannabe')){
-        $(this).css('background', 'red');
-        redPoint++;
-        $(this).addClass('active');
-        console.log("punteggio rosso: " + redPoint);
-        $('.prossa').html("punteggio rosso: " + redPoint);
-      } else {
-        $(this).css('background', 'green');
-        greenPoint++;
-        $(this).addClass('active');
-        console.log("punteggio verde: " + greenPoint);
-        $('.pverde').html("punteggio verde: " + greenPoint);
-      }
-    }
-  );
-
-
+  for(i = 0; i < 64; i++) {
+    var quadrati = $('.tablecontainer').append('<div class="square"></div>');
+  }
 
 });
+
+
+
+
+
+//   // variabile per tenere il punteggio dei Verdi VS Rossi
+//   var redPoint = 0, greenPoint = 0;
+//
+//   // cosa accade quando clicco sul quadrato
+//   $('.square').click(
+//     function () {
+//       if($(this).hasClass('active')){
+//         console.log("OH! l'hai già cliccato!");
+//       } else if($(this).hasClass('redWannabe')){
+//         $(this).css('background', 'red');
+//         redPoint++;
+//         $(this).addClass('active');
+//         console.log("punteggio rosso: " + redPoint);
+//         $('.prossa').html("punteggio rosso: " + redPoint);
+//       } else {
+//         $(this).css('background', 'green');
+//         greenPoint++;
+//         $(this).addClass('active');
+//         console.log("punteggio verde: " + greenPoint);
+//         $('.pverde').html("punteggio verde: " + greenPoint);
+//       }
+//     }
+//   );
+//
+//
+//
+// });
